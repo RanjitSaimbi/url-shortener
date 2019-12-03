19ff37ea2402dbbf60e273e6bb39f88cf42167d0
RanjitSaimbi <ranjit.saimbi@gmail.com>
2019-12-03 16:50:30 +0000

    Add tests for controller

--- spec/controllers/url_shortener_controller_spec.rb
+++ spec/controllers/url_shortener_controller_spec.rb
@@ -1,12 +1,33 @@
 require 'rails_helper'
 
 RSpec.describe UrlShortenerController, type: :controller do
-    describe 'POST #url' do
-      context 'With a valid url request' do
-        it 'returns http success' do
-          post :url, params: { }
-          expect(response).to have_http_status(:success)
-        end
+  describe 'POST #url' do
+    context 'With a valid url request' do
+      let(:request_body) { { 'url': 'http://www.farmdrop.com' } }
+
+      it 'returns http success' do
+        post :url, params: request_body
+        expect(response).to have_http_status(:success)
+      end
+
+      it 'should return an valid response' do
+        post :url, params: request_body
+        expect(response.body).to include('http://www.farmdrop.com')
+      end
+    end
+
+    context 'With an invalid url request' do
+      let(:request_body) { { 'url': 'nonsense' } }
+
+      it 'returns http bad request' do
+        post :url, params: request_body
+        expect(response).to have_http_status(:bad_request)
+      end
+
+      it 'should return an invalid response' do
+        post :url, params: request_body
+        expect(response.body).to include('Invalid')
+      end
     end
   end
 end 
--- spec/models/url_spec.rb
+++ spec/models/url_spec.rb
@@ -3,21 +3,21 @@
 require 'rails_helper'
 
 describe URL do
-    subject { described_class.new(url) }
-    let(:url) { 'https://www.farmdrop.com' }
+  subject { described_class.new(url) }
+  let(:url) { 'https://www.farmdrop.com' }
 
-    context 'when a URL class is instantiated' do
-      it 'stores a url attribute' do
-        expect(subject.url).to eq(url)
-      end
+  context 'when a URL class is instantiated' do
+    it 'stores a url attribute' do
+      expect(subject.url).to eq(url)
+    end
 
-      it 'stores a shortened url' do
-        expect(subject.short_url.length).to eq(4)
-      end
+    it 'stores a shortened url' do
+      expect(subject.short_url.length).to eq(4)
+    end
 
-      it 'can be found from the class method' do
-        subject
-        expect(URL.find(subject.short_url)).to eq(subject)
-      end
+    it 'can be found from the class method' do
+      subject
+      expect(URL.find(subject.short_url)).to eq(subject)
     end
   end
+end
